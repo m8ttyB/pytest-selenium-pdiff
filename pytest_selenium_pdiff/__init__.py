d3bec691bbac9b6a488085945fcb60b657eb3ed7
Phil Plante <unhappyrobot@gmail.com>
2016-09-01 12:39:39 -0700

    Bump version: 0.2.6 → 0.2.7

--- pytest_selenium_pdiff/__init__.py
+++ pytest_selenium_pdiff/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Phil Plante'
 __email__ = 'phil@rentlytics.com'
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 # noinspection PyUnresolvedReferences
 from .pytest_selenium_pdiff import *
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='pytest-selenium-pdiff',
-    version='0.2.6',
+    version='0.2.7',
     description="A pytest package implementing perceptualdiff for Selenium tests.",
     long_description=readme + '\n\n' + history,
     author="Phil Plante",
