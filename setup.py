2b20ba3b689b337f743fe4e001572c563649ca3b
Phil Plante <unhappyrobot@gmail.com>
2016-09-15 15:30:31 -0700

    Bump version: 0.2.8 → 0.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='pytest-selenium-pdiff',
-    version='0.2.8',
+    version='0.3.0',
     description="A pytest package implementing perceptualdiff for Selenium tests.",
     long_description=readme + '\n\n' + history,
     author="Phil Plante",
