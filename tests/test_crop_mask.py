2446688cec8a878ca72d307477f9046fd5b6fc8a
Marc Berner <marc@rentlytics.com>
2017-04-04 16:24:18 -0700

    Change mask coordinate format. Save test artifact to determine why build is failing.

--- tests/test_crop_mask.py
+++ tests/test_crop_mask.py
@@ -29,7 +29,7 @@ def test_screenshot_cropped(selenium, tmpdir):
 
 
 def test_screenshot_masked(selenium, tmpdir):
-    mask_area = [((160, 0), (350, 50))]
+    mask_area = [(160, 0, 350, 50)]
     image_name = 'mask_test'
 
     setup_selenium_session(selenium, tmpdir)
@@ -44,7 +44,7 @@ def test_screenshot_masked(selenium, tmpdir):
 
 
 def test_screenshot_multiple_mask(selenium, tmpdir):
-    mask_area = [((160, 0), (350, 50)), ((0, 50), (500, 100))]
+    mask_area = [(160, 0, 350, 50), (0, 50, 500, 100)]
     image_name = 'mask_test'
 
     setup_selenium_session(selenium, tmpdir)
